94a1e23a179d0f5d3d8e87d893d5a3185f6d9a67
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-06-23 01:24:07 +0200

    feat: Add health controller and service
    This commit adds the `health_controller.rs` and `health_service.rs` files, which contain the implementation of the health endpoint for the application. The `health_controller.rs` file defines two routes: `/health/live` and `/health/readiness`, which return the health status of the application and the database respectively. The `health_service.rs` file contains the `HealthService` struct, which is responsible for checking the health of the database. These changes are necessary to ensure the application can properly handle health checks.

---
+++ src/health/health_service.rs
@@ -0,0 +1,19 @@
+use std::sync::Arc;
+
+use sqlx::PgPool;
+
+pub struct HealthService {
+    pool: Arc<PgPool>,
+}
+
+impl HealthService {
+    pub fn new(pool: Arc<PgPool>) -> Self {
+        Self { pool }
+    }
+
+    pub async fn check_db(&self) -> Result<(), sqlx::Error> {
+        sqlx::query("SELECT 1").execute(self.pool.as_ref()).await?;
+
+        Ok(())
+    }
+}
