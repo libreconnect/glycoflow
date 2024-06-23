53a0eb1d0d481e5a8213554dccdff6c6e74ab15e
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-06-23 10:14:52 +0200

    feat: Add `Database` struct for connecting to PostgreSQL
    The code changes add a new file `database.rs` that implements the `Database` struct. This struct is responsible for connecting to a PostgreSQL database and includes a `PgPool` field for managing the database connection pool. The `new` method is implemented to create a new instance of `Database` with the provided database configuration. This addition is necessary for interacting with the PostgreSQL database in the application.

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,41 @@ use std::sync::Arc;
 use ::lapin::auth::Credentials;
 use actix_web::{web::Data, App, HttpServer};
 use clap::Parser;
+use database::Database;
 use dotenv::dotenv;
 use env::Env;
 use health::{health_controller, health_service::HealthService};
 use lapin::LapinClient;
-use sqlx::postgres::PgPoolOptions;
+use tracing::info;
 
+mod database;
 mod env;
 mod health;
 mod lapin;
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
     dotenv().ok();
     let env = Env::parse();
 
-    let addr_in = format!("{}:{}", "localhost", 3333);
+    let addr_in = format!("{}:{}", env.host, env.port);
 
     let creds = Credentials::new(env.rabbitmq_user, env.rabbitmq_password);
     let _lapin = LapinClient::new(env.rabbitmq_url, env.rabbitmq_port, creds).await?;
+    let database = Database::new(
+        env.postgres_user,
+        env.postgres_password,
+        env.postgres_url,
+        env.postgres_port,
+        env.postgres_db,
+    )
+    .await;
 
-    let database_url = format!(
-        "postgres://{}:{}@{}:{}/{}?connect_timeout=5",
-        "postgres", "postgres", "localhost", 5432, "glycoflow"
-    );
-
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&database_url)
-        .await?;
-
-    let pool = Arc::new(pool);
-
+    let pool = Arc::new(database.pool);
     let health_service = Arc::new(HealthService::new(pool.clone()));
+
+    info!("Starting server at: {}", addr_in);
 
     HttpServer::new(move || {
         App::new()
