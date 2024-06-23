67416f09ac8d00a651913853728ace60e678e018
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-06-23 10:14:29 +0200

    feat: Add environment struct for storing PostgreSQL configuration

--- src/env.rs
+++ src/env.rs
@@ -3,6 +3,11 @@ use clap::Parser;
 #[derive(Parser, Debug, Clone)]
 pub struct Env {
     #[clap(env)]
+    pub host: String,
+    #[clap(env)]
+    pub port: u16,
+    
+    #[clap(env)]
     pub rabbitmq_url: String,
     #[clap(env)]
     pub rabbitmq_password: String,
@@ -10,4 +15,15 @@ pub struct Env {
     pub rabbitmq_user: String,
     #[clap(env)]
     pub rabbitmq_port: u16,
+
+    #[clap(env)]
+    pub postgres_url: String,
+    #[clap(env)]
+    pub postgres_password: String,
+    #[clap(env)]
+    pub postgres_user: String,
+    #[clap(env)]
+    pub postgres_port: u16,
+    #[clap(env)]
+    pub postgres_db: String,
 }
