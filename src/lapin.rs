50700ca2d98fe2564c42da706cb27a3ad84adc65
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-19 10:58:30 +0200

    feat: add new method for struct LapinClient
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/lapin.rs
+++ src/lapin.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use lapin::{auth::Credentials, Channel, Connection, ConnectionProperties};
+use lapin::{auth::Credentials, Channel, Connection, ConnectionProperties, ExchangeKind, Queue};
+use lapin::options::{ExchangeDeclareOptions, QueueDeclareOptions};
+use lapin::types::{AMQPValue, FieldTable};
 
 pub struct LapinClient {
     pub conn: Connection,
@@ -29,5 +31,87 @@ impl LapinClient {
 
     pub async fn close(&self) {
         self.conn.close(200, "Goodbye").await.unwrap();
+    }
+
+    pub async fn declare_exchange(&self, name: &str, kind: ExchangeKind) -> lapin::Result<()> {
+        self
+            .channel
+            .exchange_declare(
+                name,
+                kind,
+                ExchangeDeclareOptions::default(),
+                FieldTable::default(),
+            )
+            .await
+    }
+
+    pub async fn declare_queue(&self, name: &str, durable: bool) -> lapin::Result<Queue> {
+        let mut arguments = FieldTable::default();
+
+        arguments.insert(
+            "x-queue-type".into(),
+            AMQPValue::LongString("quorum".into()),
+        );
+
+        self
+            .channel
+            .queue_declare(
+                name,
+                QueueDeclareOptions {
+                    durable,
+                    ..Default::default()
+                },
+                arguments,
+            )
+            .await
+    }
+
+    pub async fn configure_service(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let exchange_name = "glycoflow_topic_exchange";
+
+        self
+            .declare_exchange(&exchange_name, ExchangeKind::Topic)
+            .await?;
+
+        self
+            .declare_queue("glycoflow_register_access_token", true)
+            .await?;
+        self.declare_queue("glycoflow_fetch_once", true).await?;
+        self.declare_queue("glycoflow_register_data", true).await?;
+
+        self
+            .channel
+            .queue_bind(
+                "glycoflow_register_access_token",
+                &exchange_name,
+                "glycoflow.register.access_token.v1",
+                Default::default(),
+                Default::default(),
+            )
+            .await?;
+
+        self
+            .channel
+            .queue_bind(
+                "glycoflow_fetch_once",
+                &exchange_name,
+                "glycoflow.fetch.once.v1",
+                Default::default(),
+                Default::default(),
+            )
+            .await?;
+
+        self
+            .channel
+            .queue_bind(
+                "glycoflow_register_data",
+                &exchange_name,
+                "glycoflow.register.data.v1",
+                Default::default(),
+                Default::default(),
+            )
+            .await?;
+
+        Ok(())
     }
 }
