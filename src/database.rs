b2327a2bb790dac9bcb852f3c4b1c04cfd1bb5fb
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-19 10:58:18 +0200

    fix: space tabs 2
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/database.rs
+++ src/database.rs
@@ -1,28 +1,28 @@
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::{PgPool, postgres::PgPoolOptions};
 
 pub struct Database {
-    pub pool: PgPool,
+  pub pool: PgPool,
 }
 
 impl Database {
-    pub async fn new(
-        username: String,
-        password: String,
-        host: String,
-        port: u16,
-        database: String,
-    ) -> Self {
-        let database_url = format!(
-            "postgres://{}:{}@{}:{}/{}",
-            username, password, host, port, database
-        );
+  pub async fn new(
+    username: String,
+    password: String,
+    host: String,
+    port: u16,
+    database: String,
+  ) -> Self {
+    let database_url = format!(
+      "postgres://{}:{}@{}:{}/{}",
+      username, password, host, port, database
+    );
 
-        let pool = PgPoolOptions::new()
-            .max_connections(5)
-            .connect(&database_url)
-            .await
-            .expect("Failed to create pool");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to create pool");
 
-        Database { pool }
-    }
+    Database { pool }
+  }
 }
--- src/env.rs
+++ src/env.rs
@@ -2,28 +2,30 @@ use clap::Parser;
 
 #[derive(Parser, Debug, Clone)]
 pub struct Env {
-    #[clap(env)]
-    pub host: String,
-    #[clap(env)]
-    pub port: u16,
+  #[clap(env)]
+  pub host: String,
+  #[clap(env)]
+  pub port: u16,
+  #[clap(env)]
+  pub app_key: String,
 
-    #[clap(env)]
-    pub rabbitmq_url: String,
-    #[clap(env)]
-    pub rabbitmq_password: String,
-    #[clap(env)]
-    pub rabbitmq_user: String,
-    #[clap(env)]
-    pub rabbitmq_port: u16,
+  #[clap(env)]
+  pub rabbitmq_url: String,
+  #[clap(env)]
+  pub rabbitmq_password: String,
+  #[clap(env)]
+  pub rabbitmq_user: String,
+  #[clap(env)]
+  pub rabbitmq_port: u16,
 
-    #[clap(env)]
-    pub postgres_url: String,
-    #[clap(env)]
-    pub postgres_password: String,
-    #[clap(env)]
-    pub postgres_user: String,
-    #[clap(env)]
-    pub postgres_port: u16,
-    #[clap(env)]
-    pub postgres_db: String,
+  #[clap(env)]
+  pub postgres_url: String,
+  #[clap(env)]
+  pub postgres_password: String,
+  #[clap(env)]
+  pub postgres_user: String,
+  #[clap(env)]
+  pub postgres_port: u16,
+  #[clap(env)]
+  pub postgres_db: String,
 }
