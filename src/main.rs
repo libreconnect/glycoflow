c0edc8453128d3348542a853b69a8bfe736173fb
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-19 10:58:53 +0200

    feat: update main.rs
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,99 @@
 use std::sync::Arc;
 
-use ::lapin::auth::Credentials;
-use actix_web::{web::Data, App, HttpServer};
+use ::lapin::{auth::Credentials, Channel};
+use actix_web::{App, HttpServer, web::Data};
 use clap::Parser;
-use database::Database;
 use dotenv::dotenv;
+use sqlx::PgPool;
+use tracing::info;
+
+use database::Database;
+use encryptor::Encryptor;
 use env::Env;
 use handler::listen_queue;
 use health::{health_controller, health_service::HealthService};
 use lapin::LapinClient;
-use tracing::info;
 
 mod database;
+mod encryptor;
 mod env;
 mod handler;
+mod handlers;
 mod health;
 mod lapin;
+mod messages;
+
+async fn spawn_listener(
+  queue_name: String,
+  channel: Arc<Channel>,
+  pool: Arc<PgPool>,
+  encryptor: Arc<Encryptor>,
+) -> Result<(), Box<dyn std::error::Error>> {
+  tokio::spawn(async move {
+    let _ = listen_queue(&queue_name, channel.clone(), pool.clone(), encryptor).await;
+  });
+
+  Ok(())
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt::init();
-    dotenv().ok();
-    let env = Env::parse();
+  tracing_subscriber::fmt::init();
+  dotenv().ok();
+  let env = Env::parse();
 
-    let addr_in = format!("{}:{}", env.host, env.port);
+  let encryptor = Encryptor::new(env.app_key)?;
+  let encryptor = Arc::new(encryptor);
+  let addr_in = format!("{}:{}", env.host, env.port);
 
-    let creds = Credentials::new(env.rabbitmq_user, env.rabbitmq_password);
-    let lapin = LapinClient::new(env.rabbitmq_url, env.rabbitmq_port, creds).await?;
-    let database = Database::new(
-        env.postgres_user,
-        env.postgres_password,
-        env.postgres_url,
-        env.postgres_port,
-        env.postgres_db,
-    )
-    .await;
+  let creds = Credentials::new(env.rabbitmq_user, env.rabbitmq_password);
+  let lapin = LapinClient::new(env.rabbitmq_url, env.rabbitmq_port, creds).await?;
+  let database = Database::new(
+    env.postgres_user,
+    env.postgres_password,
+    env.postgres_url,
+    env.postgres_port,
+    env.postgres_db,
+  )
+      .await;
 
-    let pool = Arc::new(database.pool);
-    let health_service = Arc::new(HealthService::new(pool.clone()));
+  sqlx::migrate!("./migrations").run(&database.pool).await?;
+  lapin.configure_service().await?;
 
-    info!("Starting server at: {}", addr_in);
+  let pool = Arc::new(database.pool);
+  let health_service = Arc::new(HealthService::new(pool.clone()));
 
-    tokio::spawn(listen_queue("test_queue", lapin.channel.clone()));
+  info!("Starting server at: {}", addr_in);
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(Data::new(health_service.clone()))
-            .service(health_controller::live)
-            .service(health_controller::readiness)
-    })
-    .bind(addr_in)?
-    .run()
-    .await?;
+  spawn_listener(
+    "glycoflow_register_access_token".to_string(),
+    lapin.channel.clone(),
+    pool.clone(),
+    encryptor.clone(),
+  )
+      .await?;
 
-    Ok(())
+  spawn_listener(
+    "glycoflow_command_fetch_once".to_string(),
+    lapin.channel.clone(),
+    pool.clone(),
+    encryptor.clone(),
+  )
+      .await?;
+
+  // tokio::spawn(async move {
+  //   let _ = listen_queue("glycoflow_register_access_token", lapin.channel.clone(), pool.clone(), encryptor).await;
+  // });
+
+  HttpServer::new(move || {
+    App::new()
+        .app_data(Data::new(health_service.clone()))
+        .service(health_controller::live)
+        .service(health_controller::readiness)
+  })
+      .bind(addr_in)?
+      .workers(1)
+      .run()
+      .await?;
+  Ok(())
 }
