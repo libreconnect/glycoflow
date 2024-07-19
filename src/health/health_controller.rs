b286dab2375bd304faedbb31acd7825bcfaac4c9
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-19 10:58:37 +0200

    feat: init health module
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/health/health_controller.rs
+++ src/health/health_controller.rs
@@ -1,38 +1,38 @@
 use std::sync::Arc;
 
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, HttpResponse, Responder, web};
 
 use crate::health::{
-    health_service::HealthService,
-    model::{Health, HealthCheckResponse},
+  health_service::HealthService,
+  model::{Health, HealthCheckResponse},
 };
 
 #[get("/health/live")]
 async fn live() -> impl Responder {
-    HttpResponse::Ok().body("I'm alive!")
+  HttpResponse::Ok().body("I'm alive!")
 }
 
 #[get("/health/readiness")]
 async fn readiness(health_service: web::Data<Arc<HealthService>>) -> impl Responder {
-    let db_health = match health_service.check_db().await {
-        Ok(_) => Health {
-            healthy: true,
-            message: Some("All connections are healthy".to_string()),
-        },
-        Err(_) => Health {
-            healthy: false,
-            message: Some("Database connection failed".to_string()),
-        },
-    };
+  let db_health = match health_service.check_db().await {
+    Ok(_) => Health {
+      healthy: true,
+      message: Some("All connections are healthy".to_string()),
+    },
+    Err(_) => Health {
+      healthy: false,
+      message: Some("Database connection failed".to_string()),
+    },
+  };
 
-    let response = HealthCheckResponse {
-        database: db_health.clone(),
-    };
+  let response = HealthCheckResponse {
+    database: db_health.clone(),
+  };
 
-    // If db_health is healthy, return 200 OK, otherwise return 500 Internal Server Error
-    if db_health.healthy {
-        HttpResponse::Ok().json(response)
-    } else {
-        HttpResponse::InternalServerError().json(response)
-    }
+  // If db_health is healthy, return 200 OK, otherwise return 500 Internal Server Error
+  if db_health.healthy {
+    HttpResponse::Ok().json(response)
+  } else {
+    HttpResponse::InternalServerError().json(response)
+  }
 }
